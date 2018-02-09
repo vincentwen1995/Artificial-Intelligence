7fd2001c979e5b582183d8b93ce5fe7edbdd5e25
Coolaswa <mfavier@live.nl>
2018-02-09 16:12:52 +0100

    Added necessary classes

---
+++ DraughtsPlugin/src/nl/tue/s2id90/group88/GameState.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.tue.s2id90.group88;
+
+/**
+ *
+ * @author Michiel
+ */
+public class GameState {
+    
+    public Move getMoves(){
+        Move move = new Move();
+        
+        return move;
+    }
+}
---
+++ DraughtsPlugin/src/nl/tue/s2id90/group88/Move.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.tue.s2id90.group88;
+
+/**
+ *
+ * @author Michiel
+ */
+public class Move {
+    
+}
